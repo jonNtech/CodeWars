9868402236127113eb48a31dc41b4fb0e843741a
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-06-09 15:56:41 -0700

    Welcome to the City

---
+++ js/8kyu/Welcome-to-the-City.js
@@ -0,0 +1,12 @@
+function sayHello( name, city, state ) {
+  let nameStr = name.join(' ')
+  return `Hello, ${nameStr}! Welcome to ${city}, ${state}!`
+}
+
+
+// OR 
+
+
+// function sayHello( name, city, state ) {
+// return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+// }
