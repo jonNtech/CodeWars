3819090a47794be2e79d398a14a3ea2cbb1a496a
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-06-14 19:57:00 -0700

    Create Simple-multiplication.js

---
+++ js/8kyu/Simple-multiplication.js
@@ -0,0 +1,15 @@
+function simpleMultiplication(number) {
+    if(number % 2 === 0){
+      return number * 8
+    } else {
+      return number * 9
+    }
+}
+
+
+// OR 
+
+
+// function simpleMultiplication(n) {
+//     return n * (n % 2 ? 9 : 8);
+// }
