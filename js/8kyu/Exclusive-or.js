4f364bc81a51e16e4acf436e1a794bf179daa1cc
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-06-17 13:47:07 -0700

    Create Exclusive-or.js

---
+++ js/8kyu/Exclusive-or.js
@@ -0,0 +1,15 @@
+function xor(a, b) {
+  if ((a != b) && (a == true || b == true)){
+    return true
+  } else {
+    return false
+  }
+    
+}
+
+
+//   OR
+
+// function xor(a, b) {
+//   return a != b;
+// }
