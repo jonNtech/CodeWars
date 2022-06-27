96b7caabf0276896e8ca57a3c59135f63f58cb30
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-06-27 10:01:36 -0700

    Update jaden-casing.js

--- js/7kyu/jaden-casing.js
+++ js/7kyu/jaden-casing.js
@@ -1,3 +1,10 @@
 String.prototype.toJadenCase = function () {
-  //...
+  let result = this.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+  return result
 };
+
+//   OR
+
+// String.prototype.toJadenCase = function () {
+//   return this.replace(/(^|\s)[a-z]/g, function(x){ return x.toUpperCase(); });
+// };
