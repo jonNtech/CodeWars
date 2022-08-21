67dcae073b1e378fc4e94a89bc2f53de19d4ace3
Jon Pena <31232174+jonNtech@users.noreply.github.com>
2022-08-21 13:03:53 -0700

    Create building-a-square.js

---
+++ js/7kyu/building-a-square.js
@@ -0,0 +1,20 @@
+function generateShape(n){
+  return ("+".repeat(n)+"\n").repeat(n).trim()
+}
+
+
+
+// OR
+
+
+function generateShape(int){  
+  var ans='';
+  for (var i=0; i<int; ++i)
+  {
+    for (var j=0; j<int; ++j)
+      ans+='+';
+    if (i!=int-1)
+      ans+='\n';
+  }
+  return ans;
+}
