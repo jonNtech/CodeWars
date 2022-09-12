bd5fdc2697e630bfa91f57f1d08f1a5469f20f0b
Jon Pena <iamjonntech@gmail.com>
2022-09-11 20:14:53 -0700

    adding 7kyu

---
+++ js/7kyu/Maximum-Product.js
@@ -0,0 +1,10 @@
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+function adjacentElementsProduct(array) {
+    // max product
+  }
+
+  console.log(adjacentElementsProduct([5, 8]), 40)
+  console.log(adjacentElementsProduct([1, 2, 3]), 6)
+  console.log(adjacentElementsProduct([1, 5, 10, 9]), 90)
