09d45b0e6a98c57e4888896342f3cb496a944e67
Jon Pena <iamjonntech@gmail.com>
2022-11-09 14:00:31 -0700

    Create h-Array.diff.js

---
+++ js/6kyu/h-Array.diff.js
@@ -0,0 +1,26 @@
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+// It should remove all values from list a, which are present in list b keeping their order.
+
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+
+function arrayDiff(a, b) {
+  return a.filter(x => !b.includes(x))
+  
+}
+
+
+//OR
+
+function array_diff(a, b) {
+  return a.filter(function(x) { return b.indexOf(x) == -1; });
+}
+
+function array_diff(a, b) {
+  b = new Set(b)
+  return a.filter(v => !b.has(v))
+}
