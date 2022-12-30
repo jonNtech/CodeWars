72f420ea6a4182e79f6bc5ba0e70b9e05fd66f4f
Jon Pena <iamjonntech@gmail.com>
2022-12-30 16:25:13 -0700

    Create BeginnerReducebutGrow.cs

---
+++ js/8kyu/BeginnerReducebutGrow.cs
@@ -0,0 +1,18 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+public class Kata
+{
+  public static int Grow(int[] x)
+  {
+    int result = 1;
+    
+    for (int i = 0; i < x.Length; ++i)
+      {
+        result *= x[i];
+      } 
+    return result;
+  }
+}
