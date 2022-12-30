fbbfc4f6187538635ac5e8ecb96bd9ec0397ff35
Jon Pena <iamjonntech@gmail.com>
2022-12-30 16:26:06 -0700

    Update BeginnerReducebutGrow.cs

--- js/8kyu/BeginnerReducebutGrow.cs
+++ js/8kyu/BeginnerReducebutGrow.cs
@@ -16,3 +16,16 @@ public class Kata
     return result;
   }
 }
+
+
+
+//////////
+using System.Linq;
+
+public class Kata
+{
+  public static int Grow(int[] x)
+  {
+    return x.Aggregate((a,b) => a*b);
+  }
+}
