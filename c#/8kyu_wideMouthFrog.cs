fab1d77fc7392122d240a3b015bdfa24eb2469a9
Jon Pena <iamjonntech@gmail.com>
2023-12-15 14:02:20 -0700

    8kyu

---
+++ c#/8kyu_wideMouthFrog.cs
@@ -0,0 +1,15 @@
+public class Kata
+{
+    public static string MouthSize(string animal)
+    {
+        return animal.ToUpper() == "ALLIGATOR" ? "small": "wide";
+    }
+}
+
+//public class Kata
+//{
+//    public static string MouthSize(string animal)
+//    {
+//        return (animal.ToLower() == "alligator") ? "small" : "wide";
+//    }
+//}
