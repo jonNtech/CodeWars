05a43e9cd5d619e06734548f8b0982e623c429c8
Jon Pena <iamjonntech@gmail.com>
2024-01-09 20:05:51 -0700

    Create 8kyu_TemplateStrings.cs

---
+++ c#/8kyu_TemplateStrings.cs
@@ -0,0 +1,14 @@
+public class Templates{
+  public static string TempleStrings(string obj, string feature)
+  {
+      string result = $"{obj} are {feature}";
+        return result;
+  }
+}
+
+
+
+//public class Templates{
+  //public static string TempleStrings(string obj, string feature) =>
+    //obj + " are " + feature;
+//}
